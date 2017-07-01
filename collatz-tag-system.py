dc25acb7601e35bc765ddca19efbee1d901eb272
Derling <derling0704@gmail.com>
2017-07-01 16:28:58 -0400

    collatz sequence challenge

--- collatz-tag-system.py
+++ collatz-tag-system.py
@@ -1,3 +1,6 @@
+#src : https://www.reddit.com/r/dailyprogrammer/comments/6e08v6/20170529_challenge_317_easy_collatz_tag_system/?st=j4lpnova&sh=e73e3ebe
+#completed 7/1/17
+#simple implementation of an explicit algorithm
 def collatzTags(string):
     alphabet={'a':'bc','b':'a','c':'aaa'}
     while len(string)>1:
