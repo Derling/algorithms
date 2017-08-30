cdd4bca7e32d8a02cc890d16d397fab4a4b40985
Derling Batista <derling0704@gmail.com>
2017-08-30 07:58:45 -0400

    Fixed for other constraints for latin squares

--- latin_squares.py
+++ latin_squares.py
@@ -10,6 +10,7 @@ r/dailyprogrammer [7-08-21] Challenged #328 [Easy] Latin Squares
 """
 
 def latin_squares(n,arr):
+    base_numbers = arr[:n]
     matrix = []
     # create n x n grid using arr from left to right
     for i in range(n):
@@ -17,8 +18,10 @@ def latin_squares(n,arr):
     for i in range(len(matrix)):
         for j in range(len(matrix)):
             # check how many times a number appears in a row, if its more than
-            # once, its not a latin square
-            if matrix[i].count(matrix[i][j]) > 1:
+            # once, its not a latin square also if a number appears only on one
+            # row then it is not a latin square. 
+            if (matrix[i].count(matrix[i][j]) > 1 or 
+                    matrix[i] not in base_numbers):
                 return False
             # check if the first number of our current row occurs more than
             # once, if it does its not a latin square 
