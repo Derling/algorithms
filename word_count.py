c13b0d5d56bf6ea119ef7a266005b1193f307632
Derling Batista <derling0704@gmail.com>
2017-09-06 21:19:25 -0400

    Added docstring

--- word_count.py
+++ word_count.py
@@ -8,22 +8,24 @@ Mock Interview question, completed on own time for self improvement.
 """
 
 punctuation: list = [
-        ',',
-        '.',
-        '!',
-        '?',
-        ]
+    ',',
+    '.',
+    '!',
+    '?',
+    ]
 
 def count_words(word_arr: list) -> dict:
+    ''' count the occurences of a word in a paragraph '''
     count: dict = {}
     for word in word_arr:
         count[word] = count.get(word, 0) + 1
     return count
 
 def check_punctuation(word: str) -> str:
+    ''' check if a word ends with a punctuation and remove it '''
     return word[:-1] if word[-1] in punctuation else word
 
 if __name__ == '__main__':
-    arr : list = list(map(check_punctuation, 
-                          input("Enter a paragraph:").split()))
+    arr: list = list(map(check_punctuation,
+                         input("Enter a paragraph:").split()))
     print(count_words(arr))
