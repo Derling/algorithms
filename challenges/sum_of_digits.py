208096d6b223fa3d7fd2ea6292ef13f735318af8
Derling Batista <derling0704@gmail.com>
2017-09-14 14:07:47 -0400

    refined function

--- challenges/sum_of_digits.py
+++ challenges/sum_of_digits.py
@@ -9,11 +9,9 @@ Created on Thu Sep 14 13:51:23 2017
 
 def sum_of_digits(base, power):
     ''' return the sum of the digits of x to the power of n'''
-    count = 0
-    for digit in map(int, str(pow(base, power))):
-        count+= digit 
-    return count
+    return sum(map(int, str(pow(base, power))))
 
 if __name__ == '__main__':
+    # input is base followed by power, ie 2 1234
     x, n = map(int, input('Enter the number followed by the power:\n').split())
     print(sum_of_digits(x, n))
