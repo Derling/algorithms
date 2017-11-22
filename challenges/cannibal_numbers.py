cbab66eb065104a445197c6f4227c928d60e9c80
Derling Batista <derling0704@gmail.com>
2017-11-21 22:02:17 -0500

    Done iteratively and recursively

--- challenges/cannibal_numbers.py
+++ challenges/cannibal_numbers.py
@@ -10,44 +10,57 @@ src: https://www.reddit.com/r/dailyprogrammer/comments/76qk58/20171016_challenge
 
 """
 
-def get_cannibals(int_set, query):
-    count = 0
-    highest = max(int_set)
-    while highest >= query:
-        count += 1
-        int_set.remove(highest)
-        highest = max(int_set)
-    while len(int_set) > 1:
-        int_set.remove(min(int_set))
-        int_set[int_set.index(max(int_set))] = max(int_set) + 1
-        if max(int_set) == query:
-            count += 1
-            int_set.remove(max(int_set))
-    return count
+def cannibal_number(array, query):
+    ''' recursive solution ''' 
+    def recursion(array, query):
+        if not array: # empty array
+            return 0
+        elif len(array) == 1: # one element in array
+            return 1 if array[0] >= query else 0
+        else: # check highest integer in array
+            if array[-1] >= query:
+                # found a strong enough cannibal, remove it and do recursion
+                return 1 + cannibal_number(array[:-1], query)
+            else:
+                # cannibal too weak, make it eat the smallest integer
+                array[-1] = array[-1] + 1
+                array.pop(0)
+                return 0 + cannibal_number(array, query)
+    return recursion(sorted(array), query)
 
-def cannibals(int_set, query):
-    int_set = sorted(int_set)
-    count = 0
-    while len(int_set) > 1:
-        if int_set[-1] >= query:
-            int_set.pop()
-            count += 1
+def cannibal_numbers(array, query): 
+    ''' iterative solution ''' 
+    if not array: # empty array
+        return 0
+    if len(array) == 1: # one element in array
+        return 1 if array[0] >= query else 0
+    cannibals = 0
+    array = sorted(array)
+    for i in range(len(array)):
+        if not array: # array is empty because of loop so we should break
+            break
+        elif array[-1] >= query:
+            # highest element meets criteria, remove it and increment count
+            cannibals += 1
+            array.pop()
         else:
-            int_set[-1] = int_set[-1] + 1
-            int_set.pop(0)
-    return count if int_set[0] != query else count + 1
+            # highest integer less than query, increment it and remove smallest
+            array[-1] = array[-1] + 1
+            array.pop(0)
+    return cannibals
+
 
 if __name__ == '__main__':
-    int_set = list(map(int, input('Enter the sacrifices:\n').split()))
-    queries = list(map(int, input('Enter the queries:\n').split()))
+    int_set = [int(i) for i in input('Enter the sacrifices:\n').split()]
+    queries = [int(i) for i in input('Enter the queries:\n').split()]
     results = [[], []]
 
     for c in queries:
-        results[0].append(get_cannibals(int_set.copy(), c))
-        results[1].append(cannibals(int_set.copy(), c))
+        results[0].append(cannibal_number(int_set.copy(), c))
+        results[1].append(cannibal_numbers(int_set.copy(), c))
 
-    if results[0] == results[1]: # chekcing the integrity of both functions
+    if results[0] == results[1]: # functions produce same results
         print('For the quieres of', queries, 'using the integers', int_set,
               ', the results were,', results[0])
     else:
-        print('error with functions')
+        print('error with functions') 
