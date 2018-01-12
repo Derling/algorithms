f92321d72e462c5825c7e4f057b577d1e0a91430
Derling <derling0704@gmail.com>
2018-01-12 10:38:45 -0500

    Updated string compression problem

--- challenges/string_compression.py
+++ challenges/string_compression.py
@@ -7,21 +7,34 @@ make a function that takes in a string and returns a shrinked version of the str
 the function must also remove consequitive characters as the result of eliminating the previous characters.
 '''
 
+def str_compress(string):
+	i, j = 0, 1
+	string = list(string)
+
+	def remove_sequence(i, n):
+		while n:
+			string.pop(i)
+			n -= 1
+
+	while j < len(string):
+		if string[i] != string[j]:
+			if j - i > 2:
+				remove_sequence(i, j - i)
+				i = 0
+				j = 0
+			else:
+				i = j
+		j += 1
+
+	if string[i] == string[j - 1] and j - i > 2:
+		remove_sequence(i, j - i)
+
+	return ''.join(string)
+
 def compress(string):
 	if len(string) < 3:
 		return string
-	i, j = 0, 1
-	string = list(string)
-	while j < len(string):
-		if string[i] != string[j]:
-			if j - i >= 2:
-				string[i:] = string[j:]
-				i = 0
-				j = 0
-		j += 1
-	if string[i] == string[j - 1] and j - i >= 2:
-		string[i:] = string[j:]
-	return ''.join(string)
+	return str_compress(string)
 
 if __name__ == '__main__':
 	test = [
@@ -31,6 +44,7 @@ if __name__ == '__main__':
 		'aaaaaaaaaaaaa', # compresses to ""
 		'aaab', # compresses to "b"
 		'baaabb', # compresses to ""
+		'abccdddmmfffm'
 	]
 	for string in test:
-		print('"{0}"" compressed is "{1}"'.format(string, compress(string)))
+		print(f'\"{string}\" compresses to \"{compress(string)}\"')
