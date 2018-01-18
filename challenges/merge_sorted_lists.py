111bf450fc29e42741e9703ba4873ef2ec78a915
Derling <derling0704@gmail.com>
2018-01-18 05:28:43 -0500

    Updated merge sorted lists problem

--- challenges/merge_sorted_lists.py
+++ challenges/merge_sorted_lists.py
@@ -12,34 +12,49 @@ class ListNode(object):
          self.val = x
          self.next = None
 
-class Solution(object):
-    def mergeTwoLists(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
-        dummy = ListNode(0)
-        dummy_head = dummy
-        while l1 or l2:
-            l1_val = l2_val = None
-            if l1:
-                l1_val = l1.val
-            if l2:
-                l2_val = l2.val
-            if l1_val != None and l2_val != None:
-                if l1_val <= l2_val:
-                    dummy.next = ListNode(l1_val)
-                    l1 = l1.next
-                elif l2_val < l1_val:
-                    dummy.next = ListNode(l2_val)
-                    l2 = l2.next
-            else:
-                if l1_val != None:
-                    dummy.next = ListNode(l1_val)
-                    l1 = l1.next
-                elif l2_val != None:
-                    dummy.next = ListNode(l2_val)
-                    l2 = l2.next
-            dummy = dummy.next
-        return dummy_head.next
+def merge_two_lists(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    dummy = ListNode(0)
+    dummy_head = dummy
+    while l1 or l2:
+        l1_val = l2_val = None
+        if l1:
+            l1_val = l1.val
+        if l2:
+            l2_val = l2.val
+        if l1_val != None and l2_val != None:
+            if l1_val <= l2_val:
+                dummy.next = ListNode(l1_val)
+                l1 = l1.next
+            elif l2_val < l1_val:
+                dummy.next = ListNode(l2_val)
+                l2 = l2.next
+        else:
+            if l1_val != None:
+                dummy.next = ListNode(l1_val)
+                l1 = l1.next
+            elif l2_val != None:
+                dummy.next = ListNode(l2_val)
+                l2 = l2.next
+        dummy = dummy.next
+    return dummy_head.next
+
+if __name__ == '__main__':
+	l1 = l1_head = ListNode(0)
+	l2 = l2_head = ListNode(0)
+	l1_values = [1, 2, 3, 4]
+	l2_values = [0, 2, 3, 5, 6]
+	for v in l1_values:
+		l1.next = ListNode(v)
+		l1 = l1.next
+	for v in l2_values:
+		l2.next = ListNode(v)
+		l2 = l2.next
+	merged_list = merge_two_lists(l1_head, l2_head)
+	while merged_list:
+		print(merged_list.val, end=' ')
+		merged_list = merged_list.next
