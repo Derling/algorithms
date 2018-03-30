7c053edd8be6027b1e424d94af3cdb6aed390717
Derling <derling0704@gmail.com>
2018-03-30 19:36:25 -0400

    Updated __doc__

--- python/add_two_numbers_ll.py
+++ python/add_two_numbers_ll.py
@@ -21,43 +21,53 @@ class ListNode(object):
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
+        type l1: ListNode
+        type l2: ListNode
+        rtype: ListNode
         """
         dummy = current = ListNode(0)
-        carry_over = 0
+        carry_over = 0 # carry_over used to track if last sum was greater than 10
         while l1 or l2 or carry_over:
             if l1:
-                carry_over = carry_over + l1.val
+                carry_over += l1.val
                 l1 = l1.next
             if l2:
-                carry_over = carry_over + l2.val
+                carry_over += l2.val
                 l2 = l2.next
             current.next = ListNode(carry_over % 10)
             current = current.next
-            carry_over = int(carry_over / 10)
+            carry_over = carry_over//10
         return dummy.next
 
+def create_list(nums):
+    '''
+    type nums: int[]
+    rtype : ListNode
+    '''
+    head = dummy = ListNode(0)
+    while nums:
+        head.next = ListNode(nums.pop())
+        head = head.next
+    return dummy.next
+
 if __name__ == '__main__':
-    l1 = l1_head = ListNode(2)
-    l1.next = ListNode(4)
-    l1 = l1.next
-    l1.next = ListNode(3)
-    l2 = l2_head = ListNode(5)
-    l2.next = ListNode(6)
-    l2 = l2.next
-    l2.next = ListNode(4)
-    solution = Solution().addTwoNumbers(l1_head, l2_head)
+    list1 = create_list([3, 4, 2])
+    list2 = create_list([4, 6, 5])
+    solution = Solution().addTwoNumbers(list1, list2)
+
+
+    list_vals = []
     while solution:
-        print(solution.val)
+        list_vals.append(str(solution.val))
         solution = solution.next
+
+    # print the values of the sum of the lists ie 7 -> 0 -> 8
+    print(' -> '.join(list_vals))
 
 '''
 iterate through each linked list while there are still nodes available
 add both values togetther to get the value of the next node
 value of next node = sum modulus 10
 carry_over keeps track of whether or not the value was greater than or equal 10
-if carry_over and no nodes are available it still adds a 1 at the end of the 
-new linked list
+if carry_over and no nodes are available it still adds a 1 at the end of the linked list
 '''
