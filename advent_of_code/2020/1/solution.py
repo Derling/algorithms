3b385d8aaadf863d2895ea3b19bc000fadcd8d79
Derling Batista <derling0704@gmail.com>
2020-12-08 23:20:41 -0500

    Part 2 of day 1

--- advent_of_code/2020/1/solution.py
+++ advent_of_code/2020/1/solution.py
@@ -2,7 +2,7 @@ import os
 from typing import List
 
 
-def solution(expense_report: List[int], needed_sum: int = 2020) -> int:
+def solution_part1(expense_report: List[int], needed_sum: int = 2020) -> int:
     report_set = set(expense_report)
     for entry in report_set:
         if (other_entry := abs(entry - needed_sum)) in report_set:
@@ -10,10 +10,31 @@ def solution(expense_report: List[int], needed_sum: int = 2020) -> int:
     return None
 
 
+def solution_part2(expense_report: List[int], needed_sum: int = 2020) -> int:
+    report_set = set(expense_report)
+
+    for idx1, entry1 in enumerate(report_set):
+        for idx2, entry2 in enumerate(report_set):
+            if idx1 == idx2:
+                continue
+            current_sum = entry1 + entry2
+            if((entry3 := needed_sum - current_sum) in report_set):
+                print(entry1, entry2, entry3)
+                print(entry1 + entry2 + entry3)
+                return entry1 * entry2 * entry3
+
+    return None
+
+
 if __name__ == '__main__':
     input_path = os.path.join(os.path.dirname(__file__), 'input.txt')
     with open(input_path) as f:
         input_list = list(map(int, f.readlines()))
-        result = solution(input_list)
+        
+        result = solution_part1(input_list)
         if result:
-            print(f'Answer is {result}')
+            print(f'part 1 answer is {result}')
+        
+        result = solution_part2(input_list)
+        if result:
+            print(f'part 2 answer is {result}')
