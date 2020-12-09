68930bff66b6d179551a2aca4536f931ffdd60e6
Derling Batista <derling0704@gmail.com>
2020-12-09 00:14:04 -0500

    Add day 2 part 2

--- advent_of_code/2020/2/solution.py
+++ advent_of_code/2020/2/solution.py
@@ -1,18 +1,31 @@
 from typing import List
 
+def is_valid_toboggan_password(password_range: tuple, char: str, password: str) -> bool:
+    # logic for part 2
+    char_position1 = password[password_range[0] - 1]
+    char_position2 = password[password_range[1] - 1]
+    return (
+        char_position1 == char and char_position2 != char
+    ) or (
+        char_position1 != char and char_position2 == char
+    )
 
-def is_valid_policy(policy: str):
-    limit, char, password = policy.split()
-    char = char[0]  # remove ':'
-    min_limit, max_limit = list(map(int, limit.split('-')))
+
+def is_valid_sled_password(password_range: tuple, char: str, password: str) -> bool:
+    # logic for part 1
+    min_limit, max_limit = password_range
     char_count = password.count(char)
-    return max_limit >= char_count >= min_limit 
+    return max_limit >= char_count >= min_limit
 
 
-def solution_1(policies: List[str]) -> int:
+def solution(policies: List[str], password_validator: callable) -> int:
+    # solves for part 1 and 2(dependant on the policy function)
     count = 0
     for policy in policies:
-        count += 1 if is_valid_policy(policy) else 0
+        password_range, char, password = policy.split()
+        password_range = tuple(map(int, password_range.split('-')))
+        char = char[0]  # remove ':' at end of string
+        count += 1 if password_validator(password_range, char, password) else 0
     return count
 
 
@@ -21,5 +34,9 @@ if __name__ == '__main__':
     fpath = os.path.join(os.path.dirname(__file__), 'input.txt')
     with open(fpath) as f:
         input_list = f.readlines()
-        valid_passwords = solution_1(input_list)
+
+        valid_passwords = solution(input_list, is_valid_sled_password)
         print(f'part 1 answer is {valid_passwords}')
+
+        valid_passwords = solution(input_list, is_valid_toboggan_password)
+        print(f'part 2 answer is {valid_passwords}')
