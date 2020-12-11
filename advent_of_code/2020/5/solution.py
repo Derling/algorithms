1522cb1b84f6464cb35558971e66c6f06344cbf7
Derling Batista <derling0704@gmail.com>
2020-12-11 00:23:50 -0500

    Day 5 part 2

--- advent_of_code/2020/5/solution.py
+++ advent_of_code/2020/5/solution.py
@@ -34,11 +34,23 @@ def traverse_seat_map(seat_map: str, ) -> int:
     return row * 8 + col
 
 
-def solution(seat_designations: List[str]) -> int:
+def get_missing_id(seat_ids: List[int]) -> int:
+    sorted_seat_ids = sorted(seat_ids)
+    last_seat_idx = 1
+    while(last_seat_idx < len(seat_ids) -1):
+        seat_id = sorted_seat_ids[last_seat_idx]
+        if seat_id - sorted_seat_ids[last_seat_idx - 1] > 1:
+            return seat_id - 1
+        elif sorted_seat_ids[last_seat_idx + 1] - seat_id > 1:
+            return seat_id + 1
+        last_seat_idx += 1
+
+
+def solution(seat_designations: List[str], processing_func: callable) -> int:
     seat_ids = []
     for seat_designation in seat_designations:
         seat_ids.append(traverse_seat_map(seat_designation))
-    return max(seat_ids)
+    return processing_func(seat_ids)
 
 
 if __name__ == '__main__':
@@ -47,5 +59,8 @@ if __name__ == '__main__':
 
     with open(fpath) as f:
         input_list = f.readlines()
-        result = solution(input_list)
+        result = solution(input_list, max)
         print(f'part 1 answer is {result}')
+
+        result = solution(input_list, get_missing_id)
+        print(f'part 2 answer is {result}')
