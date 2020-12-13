d9a3515f47efe97d2bcc40ed665bc7adecd8fc45
Derling Batista <derling0704@gmail.com>
2020-12-12 22:18:22 -0500

    Day 6 part 2

--- advent_of_code/2020/6/solution.py
+++ advent_of_code/2020/6/solution.py
@@ -1,12 +1,19 @@
+from collections import Counter
 from typing import List
 
 
+def count_similar_answers(group: str) -> int:
+    people = len(group.splitlines())
+    answer_counts = Counter(''.join(group.split()))
+    return sum([1 if answer_count == people else 0 for answer_count in answer_counts.values()])
+
+
 def count_group_answers(group: str) -> int:
-    return len(set([c for c in group]))
+    return len(set([c for c in ''.join(group.split())]))
 
 
-def solution(group_answers: List[str]) -> int:
-    return sum([count_group_answers(''.join(group.split())) for group in group_answers])
+def solution(group_answers: List[str], answers_vlidator: callable) -> int:
+    return sum([answers_vlidator(group) for group in group_answers])
 
 
 if __name__ == '__main__':
@@ -15,5 +22,8 @@ if __name__ == '__main__':
 
     with open(fpath) as f:
         input_list = f.read().split('\n\n')
-        result = solution(input_list)
+        result = solution(input_list, count_group_answers)
         print(f'answer for part 1 is {result}')
+
+        result = solution(input_list, count_similar_answers)
+        print(f'answer for part 2 is {result}')
