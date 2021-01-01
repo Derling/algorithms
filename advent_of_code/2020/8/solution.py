7afd7031d31e672d743d4e08581a958eab94dc21
Derling Batista <derling0704@gmail.com>
2020-12-31 23:17:33 -0500

    Day 8 part 2

--- advent_of_code/2020/8/solution.py
+++ advent_of_code/2020/8/solution.py
@@ -1,14 +1,45 @@
 from typing import List
 
+def change_next_jpm_or_nop(commands: List[str], ignores: int) -> List[str]:
+    new_commands = list(commands)
+    ignored_changes = 0
+    for i in range(len(new_commands)):
+        current_op = new_commands[i]
+
+        if make_nop := 'jmp' in current_op or 'nop' in current_op:
+
+            if ignored_changes < ignores:
+                ignored_changes += 1
+                continue
+
+            new_commands[i] = current_op.replace('jmp', 'nop') if make_nop else current_op.replace('nop', 'jmp')
+            break
+    
+    return new_commands
+
+
+
+
+def solution2(commands: List[str]) -> int:
+    accumulator, is_infinite = solution(commands)
+    ignore_changes = 0
+    while is_infinite:
+        new_commands = change_next_jpm_or_nop(commands, ignore_changes)
+        accumulator, is_infinite = solution(new_commands)
+        ignore_changes += 1
+    return accumulator
+        
+
+
+
 def solution(commands: List[str]) -> int:
     accumulator = 0
     operations = set()
     current_op = 0
-    while True:
+    while current_op < len(commands):
         op, acc_add = commands[current_op].split()
         next_op = 1
 
-        print(current_op, op, acc_add, accumulator)
         if op == 'nop':
             acc_add = 0
         elif op == 'acc':
@@ -18,13 +49,13 @@ def solution(commands: List[str]) -> int:
              acc_add = 0
 
         if current_op + next_op in operations:
-            break
+            return accumulator, True
 
         accumulator += acc_add
         operations.add(current_op)
         current_op += next_op
 
-    return accumulator
+    return accumulator, False
 
 
 
@@ -34,5 +65,8 @@ if __name__ == '__main__':
 
     with open(fpath) as f:
         input_list = [line.strip() for line in f.readlines()]
-        result = solution(input_list)
+        result = solution(input_list)[0]
         print(f'part 1 answer is {result}')
+
+        result = solution2(input_list)
+        print(f'part 2 answer is {result}')
